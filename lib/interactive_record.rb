1926c198f50c9ad52ff4fc3737ab6b718a781a6c
blackwell <blackwellsmithiv@gmail.com>
2019-03-31 19:23:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -66,7 +66,7 @@ class InteractiveRecord
   
    def self.find_by(x)
      #binding.pry
-    sql = "SELECT * FROM #{self.table_name} WHERE #{x.keys.first} = #{x.values.first.to_s}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{x.keys.first} = '#{x.values.first}'"
     #binding.pry
     DB[:conn].execute(sql)
     #binding.pry
